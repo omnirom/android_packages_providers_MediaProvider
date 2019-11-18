0312bc2eac69ac5e9dc1b23c06c3db866ed36df1
maxwen <max.weninger@gmail.com>
2019-11-18 23:41:28 +0100

    MediaProvider: use OMNI_FINGERPRINT to store last scan tag
    Change-Id: I9295fd80d93dc51f4195a23ac94a79fff5665ad4

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -3927,7 +3927,7 @@ public class MediaProvider extends ContentProvider {
                         Context.MODE_PRIVATE);
                 final SharedPreferences.Editor editor = scanSettings.edit();
                 editor.putString(android.media.MediaScanner.LAST_INTERNAL_SCAN_FINGERPRINT,
-                        Build.FINGERPRINT);
+                        Build.OMNI_FINGERPRINT);
                 editor.apply();
             }
             mMediaScannerVolume = null;
@@ -6602,7 +6602,7 @@ public class MediaProvider extends ContentProvider {
                 }
             } else {
                 s.append(": pid=" + android.os.Process.myPid());
-                s.append(", fingerprint=" + Build.FINGERPRINT);
+                s.append(", fingerprint=" + Build.OMNI_FINGERPRINT);
             }
         }
         return s.toString();
